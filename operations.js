431beb9bd8152abaa9f862efd31560ca2360d38c
Jannine <jannine.osman@gmail.com>
2024-04-27 10:38:04 -0600

    Add all buttons to calculator

--- operations.js
+++ operations.js
@@ -1,39 +1,42 @@
-function add(num1, num2)
-{
+let num1, num2, operation;
+let buttons = document.getElementsByTagName("button");
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("mouseover", function () {
+        buttons[i].style.backgroundColor = "hotpink";
+    });
+    buttons[i].addEventListener("mouseleave", function () {
+        buttons[i].style.backgroundColor = "white";
+    });
+}
+
+
+function add(num1, num2) {
     return num1 + num2;
 }
 
-function subtract(num1, num2)
-{
+function subtract(num1, num2) {
     return num1 - num2;
 }
 
-function multiple(num1, num2)
-{
+function multiple(num1, num2) {
     return num1 * num2;
 }
 
-function divide(num1, num2)
-{
+function divide(num1, num2) {
     return num1 / num2;
 }
 
-function operate(operation, num1, num2)
-{
-    if(operation  == "+")
-    {
+function operate(operation, num1, num2) {
+    if (operation == "+") {
         return add(num1, num2);
     }
-    else if(operation == "-")
-    {
+    else if (operation == "-") {
         return subtract(num1, num2);
     }
-    else if(operation == "*")
-    {
+    else if (operation == "*") {
         return multiple(num1, num2);
     }
-    else if(operation == "/")
-    {
+    else if (operation == "/") {
         return divide(num1, num2);
     }
 }
